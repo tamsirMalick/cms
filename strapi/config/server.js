211a4b5f96f283d065204ef020275e899d40f389
Malick MBENGUE [SNT DSI/SSI/ASP] <malick.mbengue@orange-sonatel.com>
2020-11-11 18:22:13 +0000

    cms strapi

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '496144fd14a2c590565b54f63f158699'),
+    },
+  },
+});
